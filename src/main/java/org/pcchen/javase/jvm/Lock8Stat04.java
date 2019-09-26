0b9d522bb0d9e31c66847b29d84d0d3170cd375a
ceek <cpc_geek@163.com>
2019-09-26 10:32:07 +0800

    锁的8种不同场景演示--终板

--- src/main/java/org/pcchen/javase/jvm/Lock8Stat04.java
+++ src/main/java/org/pcchen/javase/jvm/Lock8Stat04.java
@@ -5,6 +5,12 @@ import java.util.concurrent.TimeUnit;
 /**
  * 锁的8种不同场景演示
  *
+ * 对象锁：在资源类中，无论有多少同步方法，同一个线程访问资源类的同一个被synchronized修饰的方法或方法体中有synchronized (this)，他就会锁住该资源类的对象
+ * 类锁：当在同步方法中添加static关键字或者在方法里使用synchronized(Demo.class)，用来定义全局锁
+ *
+ * 对象锁：只针对同一对象的有synchronid关键字修饰的方法
+ * 类锁：只针对添加static synchronized关键字修饰的方法
+ *
  * @author ceek
  * @create 2019-09-25 16:10
  **/
@@ -19,13 +25,13 @@ public class Lock8Stat04 {
 
         Phone phone2 = new Phone();
         new Thread(() -> {
-            phone1.sendEmail();
+            phone2.sendEmail();
         }).start();
     }
 }
 
 class Phone {
-    public synchronized void sendMSM() {
+    public static synchronized void sendMSM() {
         try {
             TimeUnit.SECONDS.sleep(3);
         } catch (InterruptedException e) {
